1004cb7b65f2d746f36dd5ad2346a1e566b3b0d0
valoeghese <valoeghesemods@gmail.com>
2021-02-05 23:51:56 +1300

    loop thingy arguments brr

--- solowiki.py
+++ solowiki.py
@@ -6,4 +6,22 @@ if len(sys.argv) < 2:
 
 for i in list(sys.argv)[1:]:
   print("Transpiling " + i)
+  with source as open(i if i.endswith(".md") else (i + ".md")):
+    md = f.readlines()
+  html = []
   
+  for line in md:
+    sline = line.strip()
+    
+    # basic stuff will be done better with tokens later
+    if (sline.startswith("###")):
+      html.append("<h3>" + sline[3:] + "</h3>")
+    elif (sline.startswith("##")):
+      html.append("<h2>" + sline[2:] + "</h3>")
+    elif (sline.startswith("#"):
+      html.append("<h1>" + sline[1:] + "</h1>")
+    else:
+      html.append("<p>" + sline+ "</p>")
+      
+    git commit -m "loop thingy arguments brr"
+    
--- wiki/index.md
+++ wiki/index.md
@@ -1,3 +1,11 @@
 # Main Page
 
 It loads!
+
+So # epic lol **it _w_o*ks* ** e
+
+# > epic
+
+> # epic
+
+e # e
