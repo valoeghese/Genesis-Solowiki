8d9fac08b7237f3dab6c27a175de332fb63b6e88
valoeghese <valoeghesemods@gmail.com>
2021-02-06 08:44:07 +1300

    useful debug messages

--- solowiki.py
+++ solowiki.py
@@ -11,10 +11,26 @@ if len(sys.argv) < 2:
   exit()
 
 for i in list(sys.argv)[1:]:
-  print("Transpiling " + i)
+  print("Resolving " + i)
   input = INPUT_DIR + i
-  with open(input if input.endswith(".md") else (input + ".md")) as source:
-    md = source.read().splitlines()
+  try:
+    with open(input if input.endswith(".md") else (input + ".md")) as source:
+      md = source.read().splitlines()
+  except FileNotFoundError:
+    print("Markdown Source not found.")
+
+    post = OUTPUT_DIR + (i[:-3] if i.endswith(".md") else i) + ".html"
+    if os.path.exists(post):
+      a = input("Bin HTML file found. Permanently Delete file? [Y/N] ")
+      if (a.upper() == "Y"):
+        os.remove(post)
+      print("Deleted file.")
+    else:
+      print("Bin does not exist, cannot act on the given file.")
+    continue
+  
+  print("Transpiling " + i)
+
   html = ""
   
   for line in md:
@@ -30,7 +46,7 @@ for i in list(sys.argv)[1:]:
     else:
       html += ("<p>" + sline+ "</p>")
       
-  post = OUTPUT_DIR + "e/r/c" + (i[:-3] if i.endswith(".md") else i) + ".html"
+  post = OUTPUT_DIR + (i[:-3] if i.endswith(".md") else i) + ".html"
   postc = ""
   for j in post.split("/")[:-1]:
     postc += "/" + j
